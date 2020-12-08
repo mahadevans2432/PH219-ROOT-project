3655be56feb20894042aa633aba02459ac37247a
Manan-Seth <65979792+Manan-Seth@users.noreply.github.com>
2020-12-08 11:42:08 +0530

    Update plot_pT.C
    Log graph

--- plot_pT.C
+++ plot_pT.C
@@ -1,22 +1,22 @@
 
 void plot_pT()
 {
-	TFile *f=new TFile("13TeV_CR0_RHoff.root");
+	TFile *f=new TFile("13TeV_CR0_RHoff.root");		
 	TTree *tree = (TTree*)f->Get("pytree80100");
 	Long64_t nentries = (Long64_t)tree->GetEntries();
 	 
 	Long64_t maxTrack=100000;
    
     Int_t ntrack;
-    Double_t pT[maxTrack];
+    Double_t pT[maxTrack], log_pT[maxTrack];
 
 
     tree->SetBranchAddress("ntrack",&ntrack);
     tree->SetBranchAddress("pT",&pT);
 
 	TH1D *hpt = new TH1D("hpt","Transverse momentum",100,0.0,5.0);
-	// TCanvas *c1=new TCanvas();
-	// c1.SetLogy();
+	TCanvas *c1=new TCanvas();
+	c1->SetLogy();
 
 	for(Int_t i=0; i<nentries; i++){
 
