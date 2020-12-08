2ffa67986238c78d0924275499f251faadffecfb
Manan-Seth <65979792+Manan-Seth@users.noreply.github.com>
2020-12-08 11:45:52 +0530

    Update average_pT.C
    Log graph

--- average_pT.C
+++ average_pT.C
@@ -1,33 +1,33 @@
 
-void average_pT()
+void plot_pT()
 {
-	TFile *f=new TFile("13TeV_CR0_RHoff.root");
+	TFile *f=new TFile("13TeV_CR0_RHoff.root");		
 	TTree *tree = (TTree*)f->Get("pytree80100");
 	Long64_t nentries = (Long64_t)tree->GetEntries();
 	 
 	Long64_t maxTrack=100000;
    
     Int_t ntrack;
-    Double_t pT[maxTrack];
+    Double_t pT[maxTrack], log_pT[maxTrack];
 
 
     tree->SetBranchAddress("ntrack",&ntrack);
     tree->SetBranchAddress("pT",&pT);
 
-	TH1D *av_pt = new TH1D("av_pt","Average transverse momentum",100,0.0,2.0);
+	TH1D *hpt = new TH1D("hpt","Transverse momentum",100,0.0,5.0);
+	TCanvas *c1=new TCanvas();
+	c1->SetLogy();
 
 	for(Int_t i=0; i<nentries; i++){
 
 		tree->GetEntry(i);
-		Double_t s = 0.0;
-		//tree->GetEntry(i);
+
 		for(Int_t j=0; j<ntrack; j++){
-			s = s + pT[j];
-			//hpt->Fill(pT[j]);
+			hpt->Fill(pT[j]);
 		}
-		av_pt->Fill(s/ntrack);
 		
 	}
-	av_pt->Draw();
-	av_pt->Fit("gaus");
+	hpt->Draw();
+	hpt->Fit("expo");
+	
 }
