9df473267a2abaeef10f2f33b825d197e3d96db3
Manan-Seth <65979792+Manan-Seth@users.noreply.github.com>
2020-12-09 17:53:15 +0530

    Update average_pT.C

--- average_pT.C
+++ average_pT.C
@@ -1,8 +1,8 @@
 
 void average_pT()
-{
+{	
 	TFile *f=new TFile("13TeV_CR0_RHoff.root");
-	TTree *tree = (TTree*)f->Get("pytree80100");
+	TTree *tree = (TTree*)f->Get("pytree020");
 	Long64_t nentries = (Long64_t)tree->GetEntries();
 	 
 	Long64_t maxTrack=100000;
@@ -15,9 +15,8 @@ void average_pT()
     tree->SetBranchAddress("pT",&pT);
 
 	TH1D *av_pt = new TH1D("av_pt","Average transverse momentum",100,0.0,2.0);
-	TCanvas *c1=new TCanvas();
-	c1->SetLogy();
-	for(Int_t i=0; i<nentries; i++){
+
+	for(Int_t i=1; i<nentries; i++){
 
 		tree->GetEntry(i);
 		Double_t s = 0.0;
@@ -27,8 +26,15 @@ void average_pT()
 			//hpt->Fill(pT[j]);
 		}
 		av_pt->Fill(s/ntrack);
+		av_pt->SetLineColor(kBlue);
 		
 	}
 	av_pt->Draw();
 	av_pt->Fit("gaus");
+	av_pt->SetFillColor(kGreen);
+	auto legend = new TLegend(0.1,0.7,0.48,0.9);
+   	legend->AddEntry(av_pt,"average pT","f");
+   	legend->AddEntry(gaus,"Fit","l");
+   	legend->Draw();
+   	
 }
