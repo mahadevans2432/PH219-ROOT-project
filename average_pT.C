b2f1382a159c89ab6406be1948bb5e28ebb924fa
Manan-Seth <65979792+Manan-Seth@users.noreply.github.com>
2020-12-09 20:13:15 +0530

    Update average_pT.C
    With legend

--- average_pT.C
+++ average_pT.C
@@ -2,7 +2,7 @@
 void average_pT()
 {	
 	TFile *f=new TFile("13TeV_CR0_RHoff.root");
-	TTree *tree = (TTree*)f->Get("pytree020");
+	TTree *tree = (TTree*)f->Get("pytree80100");
 	Long64_t nentries = (Long64_t)tree->GetEntries();
 	 
 	Long64_t maxTrack=100000;
@@ -15,6 +15,8 @@ void average_pT()
     tree->SetBranchAddress("pT",&pT);
 
 	TH1D *av_pt = new TH1D("av_pt","Average transverse momentum",100,0.0,2.0);
+	TCanvas *c1=new TCanvas();
+	c1->SetLogy();
 
 	for(Int_t i=1; i<nentries; i++){
 
@@ -29,9 +31,11 @@ void average_pT()
 		av_pt->SetLineColor(kBlue);
 		
 	}
+	av_pt->GetXaxis()->SetTitle("Transverse Momentum");
+    	av_pt->GetYaxis()->SetTitle("Counts");
 	av_pt->Draw();
 	av_pt->Fit("gaus");
-	av_pt->SetFillColor(kGreen);
+	av_pt->SetFillColorAlpha(kGreen-10,0.35);
 	auto legend = new TLegend(0.1,0.7,0.48,0.9);
    	legend->AddEntry(av_pt,"average pT","f");
    	legend->AddEntry(gaus,"Fit","l");
